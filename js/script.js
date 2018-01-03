08e581dc70d130b53548af0411672bb557600f18
arturoneill <mobius0919@gmail.com>
2018-01-03 12:47:33 -0500

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,18 @@
+   $(mouseover() {
+        $("#imgPiperGis").hover(
+            function() {
+                $(this).attr("src", "images/pipergisweblogo.png");
+            },
+            function() {
+                $(this).attr("src", "images/pipergisweblogo.png");
+            }                         
+        );      
+           $("#imgDailyGreen").hover(
+            function() {
+                $(this).attr("src", "images/dailygreenmouseover.png");
+            },
+            function() {
+                $(this).attr("src", "images/dailygreenweblogo.png");
+            }                         
+        ); 
+    });

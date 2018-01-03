00bd1372ac6696adacf921c29fb190c66bea4313
arturoneill <mobius0919@gmail.com>
2018-01-03 15:05:34 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,24 @@
-   $("#imgDailyGreen").hover(function() {
-                $(this).attr("src", "images/dailygreenmouseover.png");
-            },
-            function() {
-                $(this).attr("src", "images/dailygreenweblogo.png");
+   //$("#imgDailyGreen").hover(function() {
+     //           $(this).attr("src", "images/dailygreenmouseover.png");
+       //     },
+         //   function() {
+           //     $(this).attr("src", "images/dailygreenweblogo.png");
+  //  });
+
+
+    $("#dailyGreenFrame").hover(function() {
+                $("#dailyGreenToggle").fadeToggle("fast");
     });
+
+     $("#dailyGreenFrame").focus(function() {
+                $("#dailyGreenToggle").fadeToggle("fast");
+    });
+
+     $("#dailyGreenFrame").click(function() {
+                $("#dailyGreenToggle").fadeToggle("fast");
+    });
+
+
 
      $("#piperGisFrame").hover(function() {
                 $("#piperGisToggle").fadeToggle("fast");

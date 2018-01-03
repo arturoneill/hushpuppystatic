4dac39cce5fd2f2500ae616a318335359bef29ee
arturoneill <mobius0919@gmail.com>
2018-01-03 16:22:00 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,18 +6,28 @@
   //  });
 
 //index page fades
-  $("#landingRestaurantFrame").hover(function() {
+ 
+		
+	} $("#landingRestaurantFrame").hover(function() {
+      if ($(window).width() >= 767){	
                 $("#landingRestaurantToggle").fadeToggle("fast");
+      }
     });
   $("#landingRestaurantFrame").click(function() {
+     if ($(window).width() >= 767){	
                 $("#landingRestaurantToggle").fadeToggle("fast");
+     }
     });
 
   $("#landingErrandFrame").hover(function() {
+     if ($(window).width() >= 767){	
                 $("#landingErrandToggle").fadeToggle("fast");
+     }
     });
   $("#landingErrandFrame").click(function() {
+     if ($(window).width() >= 767){	
                 $("#landingErrandToggle").fadeToggle("fast");
+     }
     });
 
 //restaurant page fades

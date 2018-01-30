9daa82d0ed83da9acf0cb902f4ab7bd194d4a224
arturoneill <mobius0919@gmail.com>
2018-01-30 10:35:36 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -65,3 +65,16 @@
      $("#piperGisFrame").click(function() {
                 $("#piperGisToggle").fadeToggle("fast");
     });
+
+$('#MAPModal').on('shown.bs.modal', (function() {
+  var mapIsAdded = false;
+
+  return function() {
+    if (!mapIsAdded) {
+      $('.modal-map').html(' <iframe src="https://www.google.com/maps/d/u/0/embed?mid=1ednoEYxCJRQLIUdhxqD0z2Obl5V8qAMp" width="100%" height="300"></iframe>');
+
+      mapIsAdded = true;
+          
+    }    
+  };
+})());
